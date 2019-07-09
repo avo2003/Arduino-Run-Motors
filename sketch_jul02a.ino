aebb254a1a3cf3a0085bba01e7962b65106a9e3e
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:39:53 +0400

    Add files via upload

---
+++ sketch_jul02a.ino
@@ -0,0 +1,18 @@
+int right1 = 11;
+int right2 = 10;
+int left2 = 9;
+int left1 = 6;
+void setup()
+{
+
+  pinMode(right1, OUTPUT);
+  pinMode(right2, OUTPUT);
+  pinMode(left1, OUTPUT);
+  pinMode(left2, OUTPUT);
+  
+}
+void loop(){
+  analogWrite(right1,180);
+  analogWrite(left1,180);
+}
+
